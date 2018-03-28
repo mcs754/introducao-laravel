44a6369dfb5e45c1f577a6516e92cadb1fa1be46
Marcos Fernandes <mcs754@gmail.com>
2018-03-27 22:06:19 -0400

    Projeto salvo do pendrive

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/produtos/{descricao}', function ($descricao){
+    return "<h1>Descrição: {$descricao}</h1>";
+});
+
+Route::get('/usuarios', function (){
+    return "<h1>Listagem de Usuários</h1>";
+});
+
